fc601e354b49d93c2e734e6d9baee6e8e53e4874
Sidrit Trandafili <forge405@gmail.com>
2017-12-10 23:11:46 +0100

    View e dati dinamici - p5

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // Creiamo i dati
+    $tasks = [
+        'ripassa OOP',
+        'vedi documentazione Laravel',
+        'continua con il progetto'
+    ];
+
+    // Passiamo i dati alla view
+    return view('welcome', compact('tasks'));
 });
