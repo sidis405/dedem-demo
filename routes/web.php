dd9823a1ee515750fffccc17b4d4fcc3939118e8
Sidrit Trandafili <forge405@gmail.com>
2017-12-11 22:57:47 +0100

    Controllers - p8

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
+Route::get('tasks', 'TasksController@index');
 
-Route::get('tasks', function () {
-    $tasks = App\Task::get();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('tasks/{task}', function ($id) {
-    $task = App\Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('tasks/{task}', 'TasksController@show');
