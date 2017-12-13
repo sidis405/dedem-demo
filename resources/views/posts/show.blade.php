991f348e8cc3199bcff3bc976685ffdb9783a135
Sidrit Trandafili <forge405@gmail.com>
2017-12-13 21:58:02 +0100

    Relazioni - p15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
+<!-- Post Content -->
+<div class="col-md-9">
+    <h1> {{ $post->title }} </h1>
+    <p>
+        {{ $post->body }}
+    </p>
+    <hr>
+    <h4>Commenti</h4>
+    <ul class="list-group">
+        @foreach($post->comments as $comment)
 
-    <!-- Post Content -->
-
-          <div class="col-md-9">
-            <h1> {{ $post->title }} </h1>
-            <p>
-              {{ $post->body }}
-            </p>
-      </div>
-
-
+            <li class="list-group-item">
+                <strong> {{ $comment->created_at->diffForHumans() }} : &nbsp;</strong>
+                <div>{{ $comment->body }}</div>
+            </li>
+        @endforeach
+    </ul>
+</div>
 @stop
